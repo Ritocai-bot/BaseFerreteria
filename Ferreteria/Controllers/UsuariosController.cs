8922c2374bdd06b68d98a1eff33e44dee45e76eb
cairo <cairo@DESKTOP-QEJOEII>
2020-10-26 21:24:06 -0600

    Add project files.

---
+++ Ferreteria/Controllers/UsuariosController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ferreteria.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        // GET: Usuarios     
+        public ActionResult Leer()
+        {
+            return View();
+        }
+
+        public ActionResult Crear()
+        {
+            return View();
+        }
+    }
+}    
